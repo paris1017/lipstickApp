6292f3656612f4aac1c1012315fd80efc723d52a
Paris Guo <parisg1017@gmail.com>
2017-10-05 14:24:44 -0500

    added read.jsp page

---
+++ src/java/dbHelpers/ReadQuery.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Lipsticks;
+
+/**
+ *
+ * @author guohuinan
+ */
+public class ReadQuery {
+    private Connection conn;
+    private ResultSet results;
+    
+    public ReadQuery(){
+   
+    Properties props = new Properties();
+        InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String driver = props.getProperty("driver.name");
+        String url = props.getProperty("server.name");
+        String username = props.getProperty("user.name");
+        String passwd = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+    
+    public void doRead(){
+        
+        try {
+            String query = "Select * FROM lipstick ORDER BY itemID ASC";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            this.results = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+     public String getHTMLTable(){
+        
+        String table = "";
+        table += "<table border=1>";
+        table += "<table>";
+        
+                table += "<th>";
+                table += "ItemID";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "ProductName";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "Brand";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "Color";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "Price";
+                table += "</th>";
+                
+               
+        
+        try {
+            while(this.results.next()){
+                
+                Lipsticks lipstick = new Lipsticks();
+                lipstick.setItemID(this.results.getInt("itemID"));
+                lipstick.setProductName(this.results.getString("productName"));
+                lipstick.setBrand(this.results.getString("brand"));
+                lipstick.setColor(this.results.getString("color"));
+                lipstick.setPrice(this.results.getInt("price"));
+                
+                table += "<tr>";
+
+                table += "<td>";
+                table += lipstick.getItemID();
+                table += "</td>";
+                
+                table += "<td>";
+                table += lipstick.getProductName();
+                table += "</td>";
+                
+                table += "<td>";
+                table += lipstick.getBrand();
+                table += "</td>";
+                
+                table += "<td>";
+                table += lipstick.getColor();
+                table += "</td>";
+                
+                table += "<td>";
+                table += lipstick.getPrice();
+                table += "</td>";
+                
+               
+                
+            table += "</tr>";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+        table += "</table>";
+                
+                return table;
+    }
+    
+    
+    
+    
+    
+}
---
+++ web/read.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : read
+    Created on : Oct 4, 2017, 10:57:33 AM
+    Author     : guohuinan
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>My Lipsticks</title>
+    </head>
+    
+     <% String table = (String) request.getAttribute("table"); %> 
+    
+    <body>
+      
+       <h1>My Lipsticks</h1>
+        
+         <%= table %>
+     
+        
+    </body>
+</html>
